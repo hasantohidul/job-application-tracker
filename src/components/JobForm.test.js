198db50e5b09de7443fe7bf952d83ae1b1569f8c
tohidul9849 <128603041+tohidul9849@users.noreply.github.com>
2024-06-21 23:58:22 +0600

    Add comments to JobForm.test.js file for clarity

--- src/components/JobForm.test.js
+++ src/components/JobForm.test.js
@@ -6,11 +6,16 @@ import configureStore from "redux-mock-store";
 import JobForm from "./JobForm";
 import { addJob } from "../redux/reducers/jobReducer";
 
+// Create a mock Redux store for testing purposes
 const mockStore = configureStore([]);
 
+/**
+ * Test suite for JobForm component
+ */
 describe("JobForm Component", () => {
   let store;
 
+  // Initialize the mock store with an initial state before each test
   beforeEach(() => {
     store = mockStore({
       jobs: {
@@ -19,7 +24,11 @@ describe("JobForm Component", () => {
     });
   });
 
+  /**
+   * Test to verify that all the form inputs are rendered
+   */
   it("renders form inputs correctly", () => {
+    // Render the JobForm component with the mock store and routing context 
     render(
       <Provider store={store}>
         <MemoryRouter>
@@ -28,12 +37,19 @@ describe("JobForm Component", () => {
       </Provider>
     );
 
+    // Check if the position input is rendered
     expect(screen.getByLabelText(/position/i)).toBeInTheDocument();
+    // Check if the company input field is rendered
     expect(screen.getByLabelText(/company/i)).toBeInTheDocument();
+    // Check if status input field in rendered
     expect(screen.getByLabelText(/status/i)).toBeInTheDocument();
   });
 
+  /**
+   * Test to verify that form submission dispatches the correct action
+   */
   it("handles form submission", async () => {
+    // Render the JobForm component with the mock store and routing context
     render(
       <Provider store={store}>
         <MemoryRouter>
@@ -42,18 +58,23 @@ describe("JobForm Component", () => {
       </Provider>
     );
 
+    // Simulate user input in the position input field
     fireEvent.change(screen.getByLabelText(/position/i), {
       target: { value: "Back End Engineer" },
     });
+    // Simulate user input in the compnay input field
     fireEvent.change(screen.getByLabelText(/company/i), {
       target: { value: "Amazon" },
     });
+    // Simulate user input in the status input field
     fireEvent.change(screen.getByLabelText(/status/i), {
       target: { value: "Interview" },
     });
 
+    // Simulate form submission by clicking the "Add Job" button
     fireEvent.click(screen.getByRole("button", { name: /add job/i }));
 
+    // Wait for the action to be dispatched after clikcing the "Add Job" button and varify its payload
     await waitFor(() => {
       const actions = store.getActions();
       expect(actions).toContainEqual(

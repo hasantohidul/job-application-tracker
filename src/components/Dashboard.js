bd6c6463b0745ad5900c32922264acd1abca3f82
Mohammad Tohidul Hasan <tohidul9849@gmail.com>
2024-09-29 13:09:46 +0600

    refactor: replace Bootstrap grid Tailwind flexbox in Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,31 +1,28 @@
 import React from "react";
-import JobCard from "./JobCard";
+import JobCard from './JobCard';
 import { useSelector } from "react-redux";
-import "./Dashboard.css";
 
 /**
  * Dashboard component to display the list of job applications
- *
+ * 
  * @returns {JSX.Element} The Dashboard component
  */
 
 function Dashboard() {
-  const jobs = useSelector((state) => state.jobs.jobList);
+  const jobs = useSelector(state => state.jobs.jobList);
+
   return (
-    <div className="container">
-      <h1>Dashboard</h1>
-      <div className="row justify-content-center justify-content-md-start">
-        {jobs.map((job) => (
-          <div
-            className="col-auto mb-4 d-flex justify-content-center"
-            key={job.id}
-          >
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <h1 className="text-2xl font-bold mb-6">Dashboard</h1>
+      <div className="flex flex-wrap -mx-4 justify-center md:justify-start">
+        {jobs.map(job => (
+          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4" key={job.id}>
             <JobCard job={job} />
           </div>
         ))}
       </div>
     </div>
-  );
+  )
 }
 
 export default Dashboard;

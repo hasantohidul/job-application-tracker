f830119c3ebaf456772dcd35c9930c5a91b3a643
Mohammad Tohidul Hasan <tohidul9849@gmail.com>
2024-10-01 19:51:52 +0600

    feat: add dark mode Tailwind utilities to ThemeSwitcher

--- src/components/ThemeSwitcher.js
+++ src/components/ThemeSwitcher.js
@@ -9,7 +9,10 @@ function ThemeSwitcher() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded" onClick={toggleTheme}>
+    <button
+      className="bg-gray-500 dark:bg-gray-700 hover:bg-gray-700 dark:hover:bg-gray-900 text-white font-bold py-2 px-4 rounded"
+      onClick={toggleTheme}
+    >
       Switch to {theme === "light" ? "Dark" : "Light"}
     </button>
   );
